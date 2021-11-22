881f695852d99b04d7e9859503e4430e9fa97e40
lucas1977ar <88600221+lucas1977ar@users.noreply.github.com>
2021-11-22 18:17:03 -0300

    Add files via upload

---
+++ TareaPHP1.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$nombre = "Lucas";
+$apellido = "Dem";
+$naciEn = 1977;
+$hobbie= "Historia, Aviación";
+$editor="VS Code";
+$so="Windows 10";
+
+
+
+echo ("Soy ");
+ echo ($nombre); 
+ echo ("  ");
+ echo ($apellido); 
+ echo (", tengo  ");
+ echo (anio - $naciEn); 
+ echo (" años y mis hobbies son  ");
+ echo ($hobbie); 
+ echo ("<br>");
+ echo ("Mi editor es ");
+ echo ($editor); 
+ echo ("  y corre bajo ");
+ echo ($so); 
+
+
+?>
